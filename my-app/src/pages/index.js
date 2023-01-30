de927270a988e416adc34828b9d762cd8d0d5723
xabados <xabados@aceproject.space>
2023-01-30 15:15:11 -0600

    added cyclops img file

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,22 +1,25 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-          <Image
-      src="/../public/images/vei.webp"
-      alt="Picture of the author"
-      width={500}
-      height={500}
-    />
-
-
+      <Image
+        src="/../public/images/vei.webp"
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
+      <Image
+        src="/../public/images/cyclops.jpg"
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
     </>
-  )
+  );
 }
